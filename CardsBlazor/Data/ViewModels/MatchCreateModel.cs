d6ed24c5171fb6d715e6869bf76f73b866902288
Owen Hamer <ugfloul@gmail.com>
2020-07-05 22:34:09 +0100

    Validation for days
    Odds calculator

--- CardsBlazor/Data/ViewModels/MatchCreateModel.cs
+++ CardsBlazor/Data/ViewModels/MatchCreateModel.cs
@@ -12,10 +12,31 @@ namespace CardsBlazor.Data.ViewModels
         public int? GameId { get; set; }
 
         [Required]
+        [CustomValidator(ErrorMessage = "You must select at least 2 players")]
         public int[] StartingPlayers { get; set; }
 
         [Required]
-        [Range(1, double.MaxValue, ErrorMessage = "The stakes must be at least £1")]
+        [Range(1, 100000000, ErrorMessage = "The stakes must be at least £1 and at most £100000000")]
         public decimal EntranceFee { get; set; }
+    }
+    public class CustomValidator : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value,
+            ValidationContext validationContext)
+        {
+            var test = value as int[];
+            if (test == null)
+            {
+                return ValidationResult.Success;
+            }
+
+            if (test.Length <= 1)
+            {
+                return new ValidationResult("You need at least 2 players to start a match");
+            }
+
+            return ValidationResult.Success;
+
+        }
     }
 }
