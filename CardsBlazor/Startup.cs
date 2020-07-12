5252785cf11bbf67568fddb9cc864c22af959476
Owen Hamer <ugfloul@gmail.com>
2020-07-12 23:20:38 +0100

    Initial Support for Legacy Style Board

--- CardsBlazor/Startup.cs
+++ CardsBlazor/Startup.cs
@@ -12,6 +12,8 @@ using Microsoft.Extensions.Hosting;
 using CardsBlazor.Data;
 using CardsBlazor.Data.Entity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Logging;
 using Syncfusion.Blazor;
 
 namespace CardsBlazor
@@ -32,7 +34,12 @@ namespace CardsBlazor
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddDbContext<CardsAppContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("CardsAppContext")));
+                options
+                    .UseSqlServer(Configuration.GetConnectionString("CardsAppContext"))
+                    .ConfigureWarnings(c => c
+                        .Log((RelationalEventId.CommandExecuting, LogLevel.Debug))
+                        .Log((RelationalEventId.CommandExecuted, LogLevel.Debug))
+                    ));
             services.AddScoped<PlayerService>();
             services.AddScoped<GameService>();
             services.AddScoped<MatchService>();
@@ -42,7 +49,7 @@ namespace CardsBlazor
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MjU4NDQyQDMxMzgyZTMxMmUzMG9BMkY4MlJmWkljbjJ4RGlhTE9YaGlRNlNoUWZ4VFp4WWtVdjNFWkM1MjQ9");
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mjg1NTA2QDMxMzgyZTMyMmUzMEo4ZHJ6djJMTnpiOWlhcjB6ZXBBYU4yRlhUaG55ckowTUlUTlcvWUQ0akU9;Mjg1NTA3QDMxMzgyZTMyMmUzMEJEemticGpNeWNxczd5dTI0UlBWZitFZDl1RXkzcnBNQXBqME9LWmNoVUk9;Mjg1NTA4QDMxMzgyZTMyMmUzMG1adlE3dnhNTXhsRzdSNVg2RWJjWXlMMEFJRU5NTmxBQnRIZ2VrbURVcnM9;Mjg1NTA5QDMxMzgyZTMyMmUzMGZDaXlWdXY2bzgyY1Z5Ti8yeVhaRlA0ZWY4d3FaT2l5emRpb3hBOUw5QkU9;Mjg1NTEwQDMxMzgyZTMyMmUzMEhUL1RlR3VuQldQa2dySkJucUxjbzFBbDNuNjVTcFJxUk0xR0ZDRkVMQW89;Mjg1NTExQDMxMzgyZTMyMmUzMEV4TEJ5UmRhSy81UFdpMG9KOWxMNDRkQUJmYUcvNW8rRzBiZExCSXF6WFU9;Mjg1NTEyQDMxMzgyZTMyMmUzMG1icTFzNU9tYzF5eU1oRkEvaklyQmpUQXl5WG9kbG5FaHVQT3FWTTdVYVk9;Mjg1NTEzQDMxMzgyZTMyMmUzMG5DazFnbVlWWXU4MDc0aWxWelgwb3lMMUZ2UFJmT0o3VjBrbVZ4V29ZUFE9;Mjg1NTE0QDMxMzgyZTMyMmUzMFpqSndWQndhcHV2QVhaV05qUzZMaEhzMVMvbGZMY0xTd1Jlci9hL01GVk09;NT8mJyc2IWhia31hfWN9Z2doYmF8YGJ8ampqanNiYmlmamlmanMDHmg8OzI+NiFiYRM0PjI6P30wPD4=;Mjg1NTE1QDMxMzgyZTMyMmUzME9aNEZ0SGgyYXNOazloYUNjUGYwemozajZuSDdMV2VUcHVQcCtQd3F0Q1k9");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
